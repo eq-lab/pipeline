b7843c5a71ef538262b2af69a2a5e3aca82db955
Pavel <pavsmel@hotmail.com>
2026-07-21 15:52:36 +0300

    Fixes to waterfall calculation

--- packages/shared/src/lib.rs
+++ packages/shared/src/lib.rs
@@ -14,6 +14,7 @@ pub mod json_numeric;
 pub mod kyc_repo;
 pub mod loan_asset_price_repo;
 pub mod loan_disbursement_repo;
+pub mod loan_economics;
 pub mod loan_fee_schedule_repo;
 pub mod loan_metadata;
 pub mod loan_snapshot;
